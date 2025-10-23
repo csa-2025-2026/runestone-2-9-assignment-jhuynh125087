c221957ba08e8de9306b0fac07d1f01226e3555c
jhuynh125087 <jhuynh125@student.hbuhsd.edu>
2025-10-23 18:19:42 +0000

    problem 2

--- Main.java
+++ Main.java
@@ -4,6 +4,32 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    String word= "calculator";
+    int count = 0; // the number of times the letter has shown up the most
+    String maxLetter = " ";
+    Scanner sc = new Scanner (System.in);
+    String targetLetter = word.substring (i, i+1);
+
+    for (int i = 0; i < word.length (); i++);
+    {
+      String targetLetter = word.substring (i,i++);
+      int count = 0;
+
+      for (int = j = 0; j < word.length; j++);
+      {
+        String currentLetter = word.substring (j, j+1);
+        if (currentLetter.equals (targetLetter));
+        {
+          count ++;
+          
+        }
+
+      }
+        maxLetter = targetLetter;
+        System.out.println ("The letter " + targetLetter + "appears " + count + "time");
+
+
+    }
+
   }
 }
